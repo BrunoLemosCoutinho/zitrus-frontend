db1be95e5651b8e871e5427c62e6e4949a4b829c
Bruno Lemos <brunolemos.ssa@gmail.com>
2022-04-16 14:03:23 -0300

    configuraçao inicial de pages e Routes

---
+++ zitrus-frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <section className='login'>
+            Login
+        </section>
+    );
+}
+
+export default Login;
